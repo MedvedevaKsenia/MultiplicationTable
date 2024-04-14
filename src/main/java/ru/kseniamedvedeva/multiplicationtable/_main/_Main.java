ac58ccf64a7034faca5dc2cf0ec70041a408f33a
Ksenia <ksenia.medvedeva.1993@gmail.com>
2024-04-14 12:45:25 +0500

    app fixed

--- src/main/java/ru/kseniamedvedeva/multiplicationtable/_main/_Main.java
+++ src/main/java/ru/kseniamedvedeva/multiplicationtable/_main/_Main.java
@@ -2,13 +2,29 @@ package ru.kseniamedvedeva.multiplicationtable._main;
 
 import ru.kseniamedvedeva.multiplicationtable.service.Multiplication;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class _Main {
     public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-        System.out.println("Multiplication table:");
-        Multiplication.outputTable();
+        int num1 = 0;
+        int num2 = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 1 number (1 to 9):");
+        if (scanner.hasNextInt()) {
+            num1 = scanner.nextInt();
+            System.out.println("Enter 2 number (1 to 9):");
+            if (scanner.hasNextInt()) {
+                num2 = scanner.nextInt();
+            } else {
+                System.out.println("You entered not number!");
+            }
+        } else {
+            System.out.println("You entered not number!");
+        }
+        scanner.close();
+        if (num1 != 0 && num2 != 0) {
+            System.out.println("Multiplication table:");
+            Multiplication.outputTable(num1, num2);
+        }
     }
 }
--- src/main/java/ru/kseniamedvedeva/multiplicationtable/service/Multiplication.java
+++ src/main/java/ru/kseniamedvedeva/multiplicationtable/service/Multiplication.java
@@ -2,12 +2,13 @@ package ru.kseniamedvedeva.multiplicationtable.service;
 
 public class Multiplication {
     static int res = 0;
-    public static void outputTable() {
-    multiplyingNumbers();
+
+    public static void outputTable(int num1, int num2) {
+        multiplyingNumbers(num1, num2);
     }
 
-    private static void multiplyingNumbers() {
-        for (int i = 2; i < 10; i++) {
+    private static void multiplyingNumbers(int num1, int num2) {
+        for (int i = num1; i <= num2; i++) {
             for (int j = 1; j < 11; j++) {
                 res = i * j;
                 System.out.println(i + " x " + j + " = " + res);
