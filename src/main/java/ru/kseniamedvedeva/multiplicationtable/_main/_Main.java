d0eedd301d2a730d66b4f7230af7820b29624f68
Ksenia <ksenia.medvedeva.1993@gmail.com>
2024-04-14 12:12:20 +0500

    fish skeleton created

--- src/main/java/ru/kseniamedvedeva/multiplicationtable/_main/_Main.java
+++ src/main/java/ru/kseniamedvedeva/multiplicationtable/_main/_Main.java
@@ -1,7 +1,15 @@
 package ru.kseniamedvedeva.multiplicationtable._main;
 
+import ru.kseniamedvedeva.multiplicationtable.service.Multiplication;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class _Main {
     public static void main(String[] args) {
-
+    Scanner scanner = new Scanner(System.in);
+        System.out.println("Multiplication table:");
+        System.out.println();
+        Multiplication.outputTable();
     }
 }
--- src/main/java/ru/kseniamedvedeva/multiplicationtable/service/Multiplication.java
+++ src/main/java/ru/kseniamedvedeva/multiplicationtable/service/Multiplication.java
@@ -1,4 +1,10 @@
 package ru.kseniamedvedeva.multiplicationtable.service;
 
 public class Multiplication {
+    public static void outputTable() {
+
+    }
+    private static void multiplyingNumbers() {
+
+    }
 }
